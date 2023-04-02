40037daa3b15b4325e20a68b7be7d27c2b885b9f
brianjdowling101 <77179492+brianjdowling101@users.noreply.github.com>
2023-04-02 20:34:24 +0100

    py

--- script.py
+++ script.py
@@ -17,6 +17,46 @@ class Board:
             row, col = coord
             self.board[row][col] = 's'
 
+    def print_board(self, hide_ships):
+        print('  | ' + ' | '.join([chr(i+65) for i in range(self.size)]) + ' |')
+        print('--+' + '+'.join(['--' for i in range(self.size)]) + '--')
+        for i in range(self.size):
+            row = []
+            for j in range(self.size):
+                if self.board[i][j] == 's' and hide_ships:
+                    row.append(' ')
+                else:
+                    row.append(self.board[i][j])
+            print(str(i+1).rjust(2) + '| ' + ' | '.join(row) + ' |')
+
+    def is_valid_coord(self, row, col):
+        return row >= 0 and row < self.size and col >= 0 and col < self.size
+
+    def is_hit(self, row, col):
+        for ship in self.ships:
+            if (row, col) in ship.coords:
+                ship.hit()
+                self.board[row][col] = 'X'
+                return True
+        self.board[row][col] = '-'
+        return False
+
+class Ship:
+    def __init__(self, size):
+        self.size = size
+        self.coords = []
+        self.hits = 0
+
+    def generate_coords(self, board):
+        while True:
+            direction = random.choice(['h', 'v'])
+            if direction == 'h':
+                row = random.randint(0, board.size-1)
+                col = random.randint(0, board.size-self.size)
+                coords = [(row, col+i) for i in range(self.size)]
+            else:
+                row = random.randint(0, board.size-self.size)
+
    
 
     
